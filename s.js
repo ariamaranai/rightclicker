aa134c9d495a5db03a9c5a95a96ef01f3253686e
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-09-30 00:33:15 +0900

    Add files via upload

--- s.js
+++ s.js
@@ -1,1 +1,1 @@
-chrome.action.onClicked.addListener(e=>chrome.scripting.executeScript({target:{tabId:e.id,allFrames:!0},func:()=>addEventListener("contextmenu",e=>e.stopPropagation(),!0)}))
+chrome.action.onClicked.addListener(e=>chrome.scripting.executeScript({target:{tabId:e.id,allFrames:!0},func:()=>addEventListener("contextmenu",e => e.stopImmediatePropagation(),!0)}))
