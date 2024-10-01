6e512ecfe1a6ec4158dceab37478cc2b48aa8c61
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-10-02 07:36:48 +0900

    Update s.js

--- s.js
+++ s.js
@@ -1,1 +1,1 @@
-chrome.action.onClicked.addListener(e=>chrome.scripting.executeScript({target:{tabId:e.id,allFrames:!0},func:()=>addEventListener("contextmenu",e=>e.stopImmediatePropagation(),1),world:"MAIN"}))
+chrome.action.onClicked.addListener(e=>chrome.scripting.executeScript({target:{tabId:e.id,allFrames:!0},world:"MAIN",func:()=>addEventListener("contextmenu",e=>e.stopImmediatePropagation(),1)}))
