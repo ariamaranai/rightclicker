b223cc6bc4da24d9ce3d93916d1b8b90d15c1ad7
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-12-08 07:52:48 +0900

    Update and rename s.js to bg.js

---
+++ bg.js
@@ -0,0 +1,7 @@
+chrome.action.onClicked.addListener(
+  e=>e.url[0]!="c" && chrome.scripting.executeScript({
+    target: {tabId: e.id, allFrames: !0},
+    world: "MAIN",
+    func: ()=> addEventListener("contextmenu",e=>e.stopImmediatePropagation(),1)
+  })
+)
