2f14ad18f8546923a79cdd170485ac6e4c8ed5b1
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-04-28 22:10:18 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,11 +1,13 @@
 chrome.action.onClicked.addListener(tab => {
   let target = { tabId: tab.id, allFrames: !0 };
-  chrome.userScripts.execute({
-    target,
-    js: [{ code: 'addEventListener("contextmenu",e=>e.stopImmediatePropagation(),1),addEventListener("selectstart",e=>e.stopImmediatePropagation(),1)' }]
-  }).catch(() => 0);
-  chrome.scripting.insertCSS({
-    target,
-    css: "*{user-select:text!important}"
-  }).catch(() => 0);
+  try {
+    chrome.userScripts.execute({
+      target,
+      js: [{ code: 'addEventListener("contextmenu",e=>e.stopImmediatePropagation(),1),addEventListener("selectstart",e=>e.stopImmediatePropagation(),1)' }]
+    });
+    chrome.scripting.insertCSS({
+      target,
+      css: "*{user-select:text!important}"
+    });
+  } catch (e) {}
 });
